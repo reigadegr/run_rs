74fdf3db564bcb84aeddef3a9459726edc52f6fa
reigadegr <2722688642@qq.com>
2024-07-22 14:24:40 +0800

    启动reflex时，先用npm安装依赖

--- src/reflex_start.rs
+++ src/reflex_start.rs
@@ -3,8 +3,11 @@ use crate::{utils};
 
 pub fn reflex_start(env_name: &str) {
     if !fs::metadata("rxconfig.py").is_ok() {
-        println!("当前目录下不存在package.json文件");
+        println!("当前目录下不存在rxconfig.py文件");
         return;
     }
+    println!("reflex项目，开始使用npm安装依赖");
+    let _ = utils::run_command(format!("cd .web && {} install && cd ..", "npm.cmd"));
+    println!("安装完毕");
     let _ = utils::run_command(format!("conda activate {} && reflex run", env_name));
 }
